c3e7886e7baf49418d8dec32e1758145eef2ec00
adaniesl2 <adaniesl@eafit.edu.co>
2025-04-24 09:43:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+st.title("Tablero para dibujo")
+
+with st.sidebar: 
+  st.subheader("Board Properties")
+  drawing_mode = st.sidebar.selectbox(
+    "Drawing Tool:",
+    ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
+  )
+
+stroke_width = st.slider("Select your line width", 1, 30, 15)
+stroke_color = st.color_picker("Stroke color", "#FFFFFF")
+bg_color = "#000000"
+
+# Create a canvas component
+canvas_result = st_canvas(
+  fill_color = "rgba(255, 165, 0, 0.3)", #Fixed fill color with some opacity
+  stroke_width = stroke_width,
+  stroke_color = stroke_color,
+  background_color = bg_color,
+  height = 300,
+  width = 500,
+  drawing_mode = drawing_mode,
+  keys = "canvas",
+)
 
